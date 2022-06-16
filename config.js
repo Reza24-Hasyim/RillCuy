31af5f53a4acfe88d338c96e4636002a061cdd24
Reza24-Hasyim <74535269+Reza24-Hasyim@users.noreply.github.com>
2022-06-16 23:04:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.owner = ['6281220670449','0']
+global.owner = ['6281221653540','0']
 global.packname = 'Bot Whatsapp'
-global.author = 'TRASH'
+global.author = 'ZaGanz'
 global.sessionName = 'data'
 global.prefa = ['','!','.','#']
-global.ownername = 'KurrXd'
+global.ownername = 'RezaaCanz'
 global.sp = '>'
 global.mess = {
     admin: 'Fitur Khusus Admin Group!',
